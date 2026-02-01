d9d70fcbbfaa19e22fb60ecc48ade8f99548436c
Kateryna Koliada <6parkseolhwa6@gmail.com>
2026-02-01 18:50:30 +0100

    Fix middleware route issue

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -32,7 +32,7 @@ export const getAllNotes = async (req, res) => {
 
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findById({
+  const note = await Note.findOne({
     _id: noteId,
     userId: req.user._id,
   });
